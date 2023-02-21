28984732fae8360209c733ffbb4031e2afd92d36
VitaliyNechayev <91311597+VitaliyNevhayev@users.noreply.github.com>
2023-02-21 10:21:51 +0300

    Add files via upload

---
+++ asdasdasd/asdasdasd/Pages/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace asdasdasd.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для _1.xaml
+    /// </summary>
+    public partial class _1 : Page
+    {
+        public _1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
+            mainWindow.frame.Navigate(new _2());
+        }
+    }
+}
